96382684c765f89352234bd1694df5d1bf9791c6
Stefan Schmitt <schmittsfn@pm.me>
2021-08-17 20:54:39 +0200

    provide images to log as argument instead

--- Sources/BinaryImagesInspector/BinaryImagesInspector.swift
+++ Sources/BinaryImagesInspector/BinaryImagesInspector.swift
@@ -34,38 +34,36 @@ import MachO
 ///
 /// https://developer.apple.com/library/archive/technotes/tn2151/_index.html
 public struct BinaryImagesInspector {
-    
+
     #if arch(x86_64) || arch(arm64)
     typealias MachHeader = mach_header_64
     #else
     typealias MachHeader = mach_header
     #endif
-    
-    private static let imageNamesToLog: [String]? = []
-    
-    
+
     /// Provides binary infos that are then used with the atos command to symbolicate stack traces
-    ///
-    /// e.g. atos -arch arm64 -o [YOUR-DSYM-ID].dSYM/Contents/Resources/DWARF/[YOUR APP] -l 0x0000000000000000 0x0000000000000000
-    ///
+    /// - Parameter imageNamesToLog: an optional array of binary image names to restrict the infos to
     /// - Returns: An array of strings containing info on loaded binary name, its load address, architecture
-    public static func getBinaryImagesInfo() -> [String] {
+    /// - Note: Example:
+    ///
+    /// atos -arch arm64 -o [YOUR-DSYM-ID].dSYM/Contents/Resources/DWARF/[YOUR APP] -l 0x0000000000000000 0x0000000000000000
+    public static func getBinaryImagesInfo(imageNamesToLog: [String]? = nil) -> [String] {
         let count = _dyld_image_count()
-        
+
         var stringsToLog = [String]()
-        
+
         for i in 0..<count {
-            
+
             guard let dyld = _dyld_get_image_name(i) else { continue }
-            
+
             let dyldStr = String(cString: dyld)
             let subStrings = dyldStr.split(separator: "/")
             guard let imageName = subStrings.last else { continue }
-            
-            if let imageNamesToLog = self.imageNamesToLog {
+
+            if let imageNamesToLog = imageNamesToLog {
                 guard imageNamesToLog.contains(String(imageName)) else { continue }
             }
-            
+
             guard let uncastHeader = _dyld_get_image_header(i) else { continue }
             let machHeader = uncastHeader.withMemoryRebound(to: MachHeader.self, capacity: MemoryLayout<MachHeader>.size) { $0 }
             guard let info = NXGetArchInfoFromCpuType(machHeader.pointee.cputype, machHeader.pointee.cpusubtype) else { continue }
@@ -74,28 +72,28 @@ public struct BinaryImagesInspector {
             let logStr = "\(imageName) \(machHeader.debugDescription) - \(String(cString: archName)) - \(uuid ?? "uuid not found")"
             stringsToLog.append(logStr)
         }
-        
+
         return stringsToLog
     }
-    
+
     private static func getBinaryImageUUID(machHeader: UnsafePointer<MachHeader>) -> String? {
-        
+
         guard var header_ptr = UnsafePointer<UInt8>.init(bitPattern: UInt(bitPattern: machHeader)) else {
             return nil
         }
-        
+
         header_ptr += MemoryLayout<MachHeader>.size
-        
+
         guard var command = UnsafePointer<load_command>.init(bitPattern: UInt(bitPattern: header_ptr)) else {
             return nil
         }
-        
+
         for _ in 0..<machHeader.pointee.ncmds {
-            
+
             if command.pointee.cmd == LC_UUID {
                 guard let ucmd_ptr = UnsafePointer<uuid_command>.init(bitPattern: UInt(bitPattern: header_ptr)) else { continue }
                 let ucmd = ucmd_ptr.pointee
-                
+
                 let cuuidBytes = CFUUIDBytes(byte0: ucmd.uuid.0,
                                              byte1: ucmd.uuid.1,
                                              byte2: ucmd.uuid.2,
@@ -121,15 +119,15 @@ public struct BinaryImagesInspector {
                     return nil
                 }
                 let str = String(cString: cstr)
-                
+
                 return str
             }
-            
+
             header_ptr += Int(command.pointee.cmdsize)
             guard let newCommand = UnsafePointer<load_command>.init(bitPattern: UInt(bitPattern: header_ptr)) else { continue }
             command = newCommand
         }
-        
+
         return nil
     }
 }
